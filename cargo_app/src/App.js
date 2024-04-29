2c55bc22a35c147315b0a75ca4ba480f6b64f570
Anass Benfillous <anasse.benfillous@gmail.com>
2024-04-29 22:51:25 +0100

    update routing structure

--- cargo_app/src/App.js
+++ cargo_app/src/App.js
@@ -1,18 +1,10 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./layouts/main";
-import Login from "./pages/Auth/Login";
-
+import MainRouter from "./Router/MainRouter";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout />}></Route>
-          <Route path="login" element={<Login />}></Route>
-        </Routes>
-      </BrowserRouter>
+      <MainRouter />
     </div>
   );
 }
